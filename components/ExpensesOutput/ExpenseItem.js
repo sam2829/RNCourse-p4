af765e240c7577e3a171a159668fe7d4c33b16ed
sam2829 <samscarisbrick@outlook.com>
2025-04-28 15:58:20 +0100

    Add styles and buttons for manage expenses screen. Start building context for handling expenses data

--- components/ExpensesOutput/ExpenseItem.js
+++ components/ExpensesOutput/ExpenseItem.js
@@ -1,18 +1,31 @@
 import { Pressable, StyleSheet, Text, View } from "react-native";
 import { GlobalStyles } from "../../constants/styles";
+import { getFormattedDate } from "../../util/date";
+import { useNavigation } from "@react-navigation/native";
 
-function ExpenseItem({ description, amount, date }) {
+function ExpenseItem({ id, description, amount, date }) {
+  const navigation = useNavigation();
+
+  function expensePressHandler() {
+    navigation.navigate("ManageExpense", {
+      expenseId: id,
+    });
+  }
+
   return (
-    <Pressable>
+    <Pressable
+      onPress={expensePressHandler}
+      style={({ pressed }) => pressed && styles.pressed}
+    >
       <View style={styles.expenseItem}>
         <View>
           <Text style={[styles.textBase, styles.description]}>
             {description}
           </Text>
-          <Text style={styles.textBase}>{date.toString()}</Text>
+          <Text style={styles.textBase}>{getFormattedDate(date)}</Text>
         </View>
         <View style={styles.amountContainer}>
-          <Text style={styles.amount}>{amount}</Text>
+          <Text style={styles.amount}>{amount.toFixed(2)}</Text>
         </View>
       </View>
     </Pressable>
@@ -22,6 +35,9 @@ function ExpenseItem({ description, amount, date }) {
 export default ExpenseItem;
 
 const styles = StyleSheet.create({
+  pressed: {
+    opacity: 0.75,
+  },
   expenseItem: {
     padding: 12,
     marginVertical: 8,
@@ -50,6 +66,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     borderRadius: 4,
+    minWidth: 80,
   },
   amount: {
     color: GlobalStyles.colors.primary500,
--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -3,69 +3,6 @@ import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
 import { GlobalStyles } from "../../constants/styles";
 
-const DUMMY_EXPENSES = [
-  {
-    id: "e1",
-    description: "A pair of shoes",
-    amount: 59.99,
-    date: new Date("2023-01-23"),
-  },
-  {
-    id: "e2",
-    description: "A pair of trousers",
-    amount: 28.28,
-    date: new Date("2024-01-23"),
-  },
-  {
-    id: "e3",
-    description: "Some bananas",
-    amount: 3.99,
-    date: new Date("2025-01-23"),
-  },
-  {
-    id: "e4",
-    description: "Golf clubs",
-    amount: 499.99,
-    date: new Date("2025-04-23"),
-  },
-  {
-    id: "e5",
-    description: "Laptop",
-    amount: 789.85,
-    date: new Date("2025-04-19"),
-  },
-  {
-    id: "e6",
-    description: "Garden Furniture",
-    amount: 350.99,
-    date: new Date("2025-03-23"),
-  },
-  {
-    id: "e7",
-    description: "New flip flops",
-    amount: 23.99,
-    date: new Date("2024-05-23"),
-  },
-  {
-    id: "e8",
-    description: "Chicago Bulls Jersey",
-    amount: 99.99,
-    date: new Date("2024-12-23"),
-  },
-  {
-    id: "e9",
-    description: "Horse racing Tikets",
-    amount: 72.23,
-    date: new Date("2025-01-11"),
-  },
-  {
-    id: "e10",
-    description: "BBQ",
-    amount: 154.97,
-    date: new Date("2025-04-12"),
-  },
-];
-
 function ExpensesOutput({ expenses, expensesPeriod }) {
   return (
     <View style={styles.container}>
